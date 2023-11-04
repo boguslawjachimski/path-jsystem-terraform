32197e9c6cf57b3b55ed393d5a4c3d21c773643f
Piotr Koska <helppointit@gmail.com>
2023-11-04 11:45:20 +0100

    terraform validate i fmt

--- 001_terraform_workflow/001_write_code/digitalocean_example/main.tf
+++ 001_terraform_workflow/001_write_code/digitalocean_example/main.tf
@@ -1,14 +1,14 @@
 # to plik z naszym kodem
 
 resource "digitalocean_project" "student_project" {
-  name = "${var.projekt_name}_${var.student}"
+  name        = "${var.projekt_name}_${var.student}"
   description = "Project for ${var.student}"
-  purpose = "Test Student Project"
+  purpose     = "Test Student Project"
   environment = "Development"
 }
 
 resource "digitalocean_vpc" "student_vpc" {
-  name = "tf-${var.projekt_name}-${var.student}"
+  name        = "tf-${var.projekt_name}-${var.student}"
   description = "VPC for ${var.student}"
-  region = "fra1"
+  region      = "fra1"
 }
--- 001_terraform_workflow/001_write_code/digitalocean_example/providers.tf
+++ 001_terraform_workflow/001_write_code/digitalocean_example/providers.tf
@@ -1,8 +1,8 @@
 # ten plik tworzymy jako drugi, w tym przypadku mamy tylko jeden provider
 
 provider "digitalocean" {
-    alias = "do"
-    token = var.do_token
+  alias = "do"
+  token = var.do_token
 }
 
 provider "digitalocean" {
