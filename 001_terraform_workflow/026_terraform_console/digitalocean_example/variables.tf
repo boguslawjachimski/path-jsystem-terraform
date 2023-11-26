c796d5ee3ff08aad0df6f9cf453c2cc7f3d2fd4c
Piotr Koska <helppointit@gmail.com>
2023-11-26 19:09:38 +0100

    Update

--- 001_terraform_workflow/026_terraform_console/digitalocean_example/variables.tf
+++ 001_terraform_workflow/026_terraform_console/digitalocean_example/variables.tf
@@ -1,11 +1,8 @@
-# This is the variables.tf file
-# This file is used to define the variables that will be used in the Terraform configuration.
-
 # For coection to DigitalOcean API
+
 variable "do_token" {
   description = "DigitalOcean API token"
   type        = string
-  sensitive = true
 }
 
 variable "sufix" {
@@ -14,23 +11,47 @@ variable "sufix" {
   default     = "stf"
 }
 
+variable "droplet_count" {
+  description = "Number of droplets"
+  type        = number
+  default     = 1
+
+  validation {
+    condition     = var.droplet_count >= 1 && var.droplet_count <= 3
+    error_message = "Droplet count must be between 1 and 3"
+  }
+}
+
 variable "user_name" {
   description = "User name"
   type        = string
+  default = "Piotr"
+  sensitive = false
+
+  validation {
+    condition     = length(var.user_name) > 2 && lower(var.user_name) != "piotr"
+    error_message = "User Name musi mieć więcej niż 2 znaki i nie może być Piotr. Podaj swoje imię!"
+  }
 }
 
 variable "user_surname" {
   description = "User surname"
   type        = string
+  default = "Koska"
+
+  validation {
+    condition     = length(var.user_surname) > 2 && lower(var.user_surname) != "koska"
+    error_message = "User Surname musi mieć więcej niż 2 znaki i nie może być Koska. Podaj swoje nazwisko!"
+  }
 }
 
 variable "environment" {
   description = "Środwisko powinno być jednym z: development, staging, production"
   type        = string
-  default     = "development"
+  default     = "DEVELOPMENT"
 
   validation {
-    condition     = contains(["development", "staging", "production"], var.environment)
+    condition     = contains(["development", "staging", "production"],  lower(var.environment))
     error_message = "Środowisko musi być jednym z: development, staging, production"
   }
 
@@ -40,4 +61,16 @@ variable "region" {
   description = "Region"
   type        = string
   default     = "fra1"
+}
+
+variable "droplet_count" {
+  description = "Number of droplets"
+  type        = number
+  default     = 1
+
+  validation {
+    condition     = var.droplet_count > 0 && var.droplet_count < 10
+    error_message = "Droplet count must be between 1 and 10"
+  }
+  
 }
